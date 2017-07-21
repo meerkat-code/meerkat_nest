0612f8209af757ca73ffe6db4320a61e2e3c16fa
jyri <Jyri Soppela>
2017-07-21 11:34:27 +0100

    sns interface

--- meerkat_nest/message_service.py
+++ meerkat_nest/message_service.py
@@ -3,11 +3,15 @@ Data resource for interacting with Amazon Simple Queue Service
 """
 import boto3
 import json
+import logging
 
 from meerkat_nest import config
 
-sqs_client = boto3.client('sqs', region_name='eu-west-1')
-sts_client = boto3.client('sts', region_name='eu-west-1')
+region_name = 'eu-west-1'
+sqs_client = boto3.client('sqs', region_name=region_name)
+sts_client = boto3.client('sts', region_name=region_name)
+sns_client = boto3.client('sns', region_name=region_name)
+
 
 def get_account_id():
     """
@@ -19,7 +23,8 @@ def get_account_id():
     account_id = sts_client.get_caller_identity()["Account"]
     return account_id
 
-def create_queue_name(data_entry):
+
+def get_queue_name(data_entry):
     """
     Creates a queue name based on organization, entry content
     and entry subcontent
@@ -28,6 +33,31 @@ def create_queue_name(data_entry):
         automatically generated SQS queue name\n
     """
     return 'nest-queue-' + config.country_config['country_name'].lower() + '-' + data_entry['content'] + '-' + data_entry['formId']
+
+
+def get_dead_letter_queue_name(data_entry):
+    """
+    Creates a dead letter queue name for an organization and content type
+    
+    Returns:\n
+        automatically generated SQS queue name\n
+    """
+    return 'nest-dead-letter-queue-' + config.country_config['country_name'].lower() + '-' + data_entry['content']
+
+
+def create_sns_topic():
+    """
+    Creates a SNS topic based on the country configurations
+
+    Returns:\n
+        Topic ARN
+    """
+    topic_arn = sns_client.create_topic(
+        Name='nest-incoming-topic-' + config.country_config['country_name'].lower()
+    )
+
+    return topic_arn
+
 
 def get_queue_url(queue_name):
     """
@@ -42,24 +72,22 @@ def get_queue_url(queue_name):
     )
     return response['QueueUrl']
 
+
 def create_queue(data_entry):
     """
     Creates a queue based on the given data entry
-    
-    Returns:\n
-        uuid for the PK of the raw data row\n
     """
     try:
-        queue_name = create_queue_name(data_entry)
+        queue_name = get_queue_name(data_entry)
         response = sqs_client.create_queue(
             QueueName=queue_name
         )
-        return True
-    except AssertionError as e:
+    except Exception as e:
         message = e.args[0]
         message += "\Message queue creation failed."
         e.args = (message,)
         raise
+
 
 def send_data(data_entry):
     """
@@ -78,21 +106,36 @@ def send_data(data_entry):
         e.args = (message,)
         raise
 
-    queue_name = create_queue_name(data_entry)
-    try:
-        response = sqs_client.send_message(
-            QueueUrl=get_queue_url(create_queue_name(data_entry)),
-            MessageBody=json.dumps(data_entry['data'])
-        )
-        print('DEBUG: ' + str(response))
-    except Exception as e:
-        raise
+    response = sqs_client.send_message(
+        QueueUrl=get_queue_url(get_queue_name(data_entry)),
+        MessageBody=json.dumps(data_entry['data'])
+    )
+    logging.debug("SQS send message response " + str(response))
+
+    notify_sns(get_queue_name(data_entry), get_dead_letter_queue_name(data_entry))
+
 
 def receive_data(queue_name,n=100):
     return_set = []
 
     for i in range(1,n):
         sqs_client.receive_message(
-            QueueUrl='string',
+            QueueUrl=get_queue_url(queue_name),
             )
     return return_set
+
+
+def notify_sns(queue_name, dead_letter_queue_name):
+    """
+    Notify Simple Notification service that queue has new data
+    """
+    message = {
+        "queue":queue_name,
+        "dead-letter-queue":dead_letter_queue_name
+    }
+
+    response = sns_client.publish(
+        TopicArn=create_sns_topic(),
+        Message=json.dumps(message)
+    )
+    logging.debug("SNS notification response " + str(response))
