043dab272594a650a7cb13fcb76b368c2d724501
jyri <Jyri Soppela>
2017-08-31 21:18:06 +0300

    API to deactivate data and send deletion message downstream

--- meerkat_nest/resources/deactivate_data.py
+++ meerkat_nest/resources/deactivate_data.py
@@ -21,7 +21,7 @@ db_url = os.environ['MEERKAT_NEST_DB_URL']
 engine = create_engine(db_url)
 
 
-class UploadData(Resource):
+class DeactivateData(Resource):
     """
     Receives JSON data and stores it in Meerkat Nest database
     
@@ -30,11 +30,11 @@ class UploadData(Resource):
     """
 
     def get(self):
-        return "upload data GET"
+        return "deactivate data GET"
 
     def post(self):
 
-        logging.debug("received upload request")
+        logging.debug("received deactivation request")
         logging.debug(str(request.headers))
         data_entry = request.get_json()
         logging.debug(str(data_entry))
@@ -48,26 +48,19 @@ class UploadData(Resource):
             return Response(json.dumps({"message": msg}),
                             status=400,
                             mimetype='application/json')
-        # Upload the data entry in to raw data storage
+        # Deactivate the data entry in the raw storage
         try:
-            uuid_pk = upload_to_raw_data(data_entry)
-            # data_entry['uuid'] = uuid_pk
-        except AssertionError as e:
-            msg = "Raw input type '" + data_entry['content'] + "' is not supported"
-            logging.error(msg)
-            return Response(json.dumps({"message": msg}),
-                            status=400,
-                            mimetype='application/json')
+            deactivate_data_entry(data_entry)
         except Exception as e:
-            msg = "Error in uploading data: " + e.args[0]
+            msg = "Error in deactivating data: " + e.args[0]
             logging.error(msg)
             return Response(json.dumps({"message": msg}),
                             status=502,
                             mimetype='application/json')
 
-        # Process the data entry
+        # Delete the data entry from the processed data
         try:
-            processed_data_entry = process(data_entry)
+            delete_from_processed(data_entry)
         except AssertionError as e:
             msg = "Data type '" + data_entry['formId'] + "' is not supported for input type '"\
                 + data_entry['content'] + "'"
@@ -76,131 +69,57 @@ class UploadData(Resource):
                             status=400,
                             mimetype='application/json')
 
-        # Send processed data forward to the cloud
+        # Send deactivation message to the cloud
         try:
-            sent = message_service.send_data(processed_data_entry)
+            data_entry_delete = message_service.send_deactivation_message(data_entry)
         except AssertionError as e:
-            msg = "Error in forwarding data to message queue: " + str(e)
+            msg = "Error in sending deactivation message to message queue: " + str(e)
             logging.error(msg)
             return Response(json.dumps({"message": msg}),
                             status=502,
                             mimetype='application/json')
 
-        logging.debug("processed upload request")
-        return processed_data_entry
+        logging.debug("processed deactivation request")
+        return Response(json.dumps(data_entry_delete),
+                        status=200,
+                        mimetype='application/json')
 
 
-def upload_to_raw_data(data_entry):
-    """
-    Stores raw data in Meerkat Nest database
-    
-    Returns:\n
-        uuid for the PK of the raw data row\n
-    """
+def deactivate_data_entry(data_entry):
 
-    uuid_pk = str(uuid.uuid4())
-
-    insert_row = None
-
-    if data_entry['content'] == 'record':
-        insert_row = model.rawDataOdkCollect.__table__.insert().values(
-                uuid=uuid_pk,
-                received_on=datetime.datetime.now(),
-                active_from=datetime.datetime.now(),
-                authentication_token=data_entry['token'],
-                content=data_entry['content'],
-                formId=data_entry['formId'],
-                formVersion=data_entry['formVersion'],
-                data=data_entry['data']
-        )
-
-    assert insert_row is not None, "Content handling not implemented"
-
-    connection = engine.connect()
-    connection.execute(insert_row)
-    connection.close()
-
-    return uuid_pk
-
-
-def process(data_entry):
-    """
-    Processes raw data and stores the processed data entry in in Meerkat Nest database
-    
-    Returns:\n
-        processed data_entry if processing was successful, False otherwise
-    """
-    processed_data_entry = restructure_aggregate_data(data_entry)
-    processed_data_entry = scramble_fields(processed_data_entry)
-    processed_data_entry = format_field_keys(processed_data_entry)
-
-    assert data_entry['content'] in ['form', 'record'], "Content not supported"
-    assert data_entry['formId'] in config.country_config['tables'], "Form not supported"
-
-    insert_row = model.data_type_tables[processed_data_entry['formId']].__table__.insert().values(
-           uuid=processed_data_entry['uuid'],
-           data=processed_data_entry['data']
-        )
+    deactivate_row = model.RawDataOdkCollect.__table__.insert().values(
+            active_until=datetime.datetime.now()
+    ).where(
+        model.RawDataOdkCollect.uuid == data_entry['uuid']
+    )
 
     try:
         connection = engine.connect()
-        connection.execute(insert_row)
+        connection.execute(deactivate_row)
         connection.close()
-        return processed_data_entry
+        return data_entry
     except Exception as e:
         raise
 
 
-def restructure_aggregate_data(data_entry):
-    """
-    Restructures data from aggregate JSON feed
-    
-    Returns:\n
-        restructured data entry
-    """
+def delete_from_processed(data_entry):
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    restructured_data = data_entry['data'][0]
-    data_entry['data'] = restructured_data
+    delete_row_query = session.query(model.data_type_tables[data_entry['formId']])\
+        .filter(model.data_type_tables[data_entry['formId']].uuid == data_entry['uuid']).all()
 
-    data_entry['uuid'] = data_entry['data']['*meta-instance-id*']
+    assert len(delete_row_query) > 0, "No record with uuid " + data_entry['uuid'] + " found"
+    assert len(delete_row_query) < 2, "Multiple records with uuid " + data_entry['uuid'] + " found"
 
-    return data_entry
-
-
-def scramble_fields(data_entry):
-    """
-    Scrambles fields in data entry based on configurations
-    
-    Returns:\n
-        data entry structure with scrambled fields
-    """
-
-    data_entry_scrambled = data_entry
+    delete_row = delete_row_query[0]
 
     try:
-        fields = config.country_config['scramble_fields'][data_entry['formId']]
-    except KeyError as e:
-        return data_entry_scrambled
-
-    for field in fields:
-        if field in data_entry_scrambled['data'].keys():
-            data_entry_scrambled['data'][field] = scramble(data_entry_scrambled['data'][field])
-
-    return data_entry_scrambled
+        session.delete(delete_row)
+        session.commit()
+        session.flush()
+    except Exception as e:
+        raise
 
 
-def format_field_keys(data_entry):
-    """
-    Formats the field names in the data entry
-    
-    Returns:\n
-        data entry structure with formatted field namess
-    """
 
-    for key in data_entry['data'].keys():
-        new_key = format_form_field_key(key)
-        if new_key != key:
-            data_entry['data'].update({new_key: data_entry['data'][key]})
-            data_entry['data'].pop(key)
-
-    return data_entry 
