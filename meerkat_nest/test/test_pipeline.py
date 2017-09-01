54679397e92cc9a4dbfdbd13b6b7271167b4b059
jyri <Jyri Soppela>
2017-09-01 15:36:42 +0300

    remove SQS data fetching now that data gets forwarded

--- meerkat_nest/test/test_pipeline.py
+++ meerkat_nest/test/test_pipeline.py
@@ -55,15 +55,15 @@ class MeerkatNestPipelineTest(unittest.TestCase):
         self.assertEqual(post_response.status_code, 200)
 
         # check that data ended up in queue
-        queue_name = meerkat_nest.message_service.get_queue_name(payload)
-        return_set = meerkat_nest.message_service.receive_data(queue_name, n=1)
+#        queue_name = meerkat_nest.message_service.get_queue_name(payload)
+#        return_set = meerkat_nest.message_service.receive_data(queue_name, n=1)
 
-        form_payload = json.loads(return_set[0]['Messages'][0]['Body'])
-        uuid_from_sqs = form_payload['data']['instanceID']
+#        form_payload = json.loads(return_set[0]['Messages'][0]['Body'])
+#        uuid_from_sqs = form_payload['data']['instanceID']
 
         # TODO: send receipt for SQS
 
-        self.assertEqual(uuid, uuid_from_sqs)
+#        self.assertEqual(uuid, uuid_from_sqs)
 
         # check SNS via email
 
