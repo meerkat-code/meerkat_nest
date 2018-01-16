96ca622829ceca7f6f24288a176733af55b1628f
Tomasz Sabała <sabala.tomek@gmail.com>
2018-01-16 15:01:00 +0100

    Fixing commented out init.py.

--- meerkat_nest/__init__.py
+++ meerkat_nest/__init__.py
@@ -5,24 +5,24 @@ from flask import Flask
 from flask_restful import Api
 import os
 
-# from meerkat_nest import setup_database
+from meerkat_nest import setup_database
 from meerkat_nest import model
 
-# from meerkat_nest.resources.upload_data import UploadData
-# from meerkat_nest.resources.amend_data import amendData
+from meerkat_nest.resources.upload_data import UploadData
+from meerkat_nest.resources.amend_data import amendData
 
 # Create the Flask app
 app = Flask(__name__)
 api = Api(app)
 app.config.from_object('meerkat_nest.config.Config')
 
-# db_url = os.environ['MEERKAT_NEST_DB_URL']
-# setup_database.create_db(db_url, model.Base, drop=False)
+db_url = os.environ['MEERKAT_NEST_DB_URL']
+setup_database.create_db(db_url, model.Base, drop=False)
 
 
 @app.route('/')
 def nest_root_url():
     return "Meerkat Nest"
 
-# api.add_resource(UploadData, "/upload")
-# api.add_resource(amendData, "/amend")
+api.add_resource(UploadData, "/upload")
+api.add_resource(amendData, "/amend")
