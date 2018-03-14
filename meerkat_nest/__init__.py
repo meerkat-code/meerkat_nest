ab2371c7433d7b0bcecbcf9e4d4c304c30fe413a
jyri <Jyri Soppela>
2018-03-14 10:56:14 +0200

    allow importing app without configuring DB

--- meerkat_nest/__init__.py
+++ meerkat_nest/__init__.py
@@ -4,6 +4,7 @@ Root Flask app for the Meerkat Nest.
 from flask import Flask
 from flask_restful import Api
 import os
+import logging
 
 from meerkat_nest import setup_database
 from meerkat_nest import model
@@ -16,9 +17,11 @@ app = Flask(__name__)
 api = Api(app)
 app.config.from_object('meerkat_nest.config.Config')
 
-db_url = os.environ['MEERKAT_NEST_DB_URL']
-setup_database.create_db(db_url, model.Base, drop=False)
-
+db_url = os.environ.get('MEERKAT_NEST_DB_URL', None)
+if db_url:
+    setup_database.create_db(db_url, model.Base, drop=False)
+else:
+    logging.error("No database configured!")
 
 @app.route('/')
 def nest_root_url():
