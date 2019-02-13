meerkat-code/meerkat_nest

commit c011cab02be41c6a2b3baf1a77c29e3131786324

Take a hard copy of the dictionary keys

meerkat_nest/resources/upload_data.py
@@ -117,7 +117,7 @@ class UploadData(Resource):
 def upload_to_raw_data(data_entry):
     """
     Stores raw data in Meerkat Nest database
-    
+
     Returns:\n
         uuid for the PK of the raw data row\n
     """
@@ -165,7 +165,7 @@ def store_processed_data(data_entry):
 def process(data_entry):
     """
     Processes raw data and stores the processed data entry in in Meerkat Nest database
-    
+
     Returns:\n
         processed data_entry if processing was successful, False otherwise
     """
@@ -186,7 +186,7 @@ def process(data_entry):
 def restructure_aggregate_data(data_entry):
     """
     Restructures data from aggregate JSON feed
-    
+
     Returns:\n
         restructured data entry
     """
@@ -232,7 +232,7 @@ def process_patient_id(data_entry):
 def scramble_fields(data_entry):
     """
     Scrambles fields in data entry based on configurations
-    
+
     Returns:\n
         data entry structure with scrambled fields
     """
@@ -270,16 +270,16 @@ def hash_fields(data_entry):
 def format_field_keys(data_entry):
     """
     Formats the field names in the data entry
-    
+
     Returns:\n
         data entry structure with formatted field namess
     """
 
     rename_fields = config.country_config.get('rename_fields',
                                               {}).get(data_entry['formId'], {})
-    character_replacements = config.country_config.get('replace_characters', 
+    character_replacements = config.country_config.get('replace_characters',
                                                        {}).get(data_entry['formId'], [])
-    data_fields = data_entry['data'].keys()
+    data_fields = list(data_entry['data'].keys())
 
     # Perform character replacements to all fields
     for characters in character_replacements:

commit a75ae53dfd06d28e862f9b5da97f397662608990

fix field name map issue caused by commit c011cab

meerkat_nest/resources/upload_data.py
@@ -289,6 +289,8 @@ def format_field_keys(data_entry):
                 data_entry['data'].pop(key)
 
     # Perform key replacements
+    data_fields = list(data_entry['data'].keys())
+
     for key in rename_fields:
         if key in data_fields:
             data_entry['data'][rename_fields[key]] = data_entry['data'][key]